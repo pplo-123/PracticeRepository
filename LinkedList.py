53e5e153ae5ffe33af497e4bb1c4b61d0b69683c
Shubham Verma <shubhkv@amazon.com>
2021-02-18 21:45:44 +0530

    Adding LinkedList Python File

---
+++ LinkedList.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__self(self):
+        self.head = None
+
+# create an instance of class
+
+ll = LinkedList()
+ll.head = Node(1)
+ll.head.next = Node(2)
+
+
+
+
